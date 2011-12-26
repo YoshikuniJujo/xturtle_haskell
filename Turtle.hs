01e85401cac26f9c469ca80594abb12b652cc471
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-12-26 13:39:21 +0900

    yet many bugs

--- Turtle.hs
+++ Turtle.hs
@@ -5,7 +5,7 @@ import Data.Bits
 import Graphics.X11.Xlib.Extras
 import Control.Monad.Tools
 import Data.Char
-import Control.Arrow
+import Control.Arrow (second)
 import Control.Concurrent
 import System.IO.Unsafe
 import Data.IORef
@@ -13,8 +13,14 @@ import Data.IORef
 kameStat :: IORef (Int, (Position, Position), Double)
 kameStat = unsafePerformIO $ newIORef (0, (150, 150), 1)
 
+drawRef :: IORef (IO ())
+drawRef = unsafePerformIO $ newIORef $ return ()
+
 xstatRef :: IORef (Display, Window, GC)
 xstatRef = unsafePerformIO $ newIORef (undefined, undefined, undefined)
+
+initTurtle :: IO ()
+initTurtle = makeWindow >> forward 0
 
 makeWindow :: IO (Display, Window, GC)
 makeWindow = do
@@ -94,11 +100,13 @@ setForegroundColor (dpy, _, gc) clr = setForeground dpy gc clr
 main = do
 	xstat@(dpy, _, _) <- makeWindow
 	doWhile (0, (150, 150)) $ \(b, (x, y)) -> allocaXEvent $ \e -> do
+		drawLines <- readIORef drawRef
 		nextEvent dpy e
 		ev <- getEvent e
 		case ev of
 			ExposeEvent {} -> do
-				displayTurtle xstat 150 150 0 0.4
+				drawLines
+				displayTurtle xstat x y b 0.4
 				return ((b, (x, y)), True)
 			KeyEvent {} -> do
 				ch <- getKeyChar xstat ev
@@ -106,7 +114,7 @@ main = do
 					't' -> fmap (flip (,) (x, y))
 						$ kameTurn xstat b 30 (x, y) 0.4
 					' ' -> fmap ((,) b)
-						$ kameForward xstat b (x, y) 100 0.4
+						$ kameForward xstat b (x, y) 50 0.4
 					_ -> return (b, (x, y))
 				return (ns, ch /= 'q')
 	closeWindow xstat
@@ -118,12 +126,26 @@ forward dx = do
 	(x', y') <- kameForward xstat b (x, y) dx $ s * 0.3
 	writeIORef kameStat (b, (x', y'), s)
 
+backward :: Position -> IO ()
+backward = forward . negate
+
+left :: Int -> IO ()
+left = turn . negate
+
 turn :: Int -> IO ()
 turn d = do
 	xstat <- readIORef xstatRef
 	(b, (x, y), s) <- readIORef kameStat
 	b' <- kameTurn xstat b d (x, y) $ s * 0.3
 	writeIORef kameStat (b', (x, y), s)
+
+clear :: IO ()
+clear = do
+	(dpy, win, _) <- readIORef xstatRef
+	clearWindow dpy win
+	flush dpy
+	writeIORef drawRef $ return ()
+	forward 0
 
 shapeSize :: Double -> IO ()
 shapeSize ns = do
@@ -140,29 +162,35 @@ kameForward ::
 	(Display, Window, GC) -> Int -> (Position, Position) -> Position -> Double
 		-> IO (Position, Position)
 kameForward xstat@(dpy, win, gc) d0 (x0, y0) len s = do
+	drawLines <- readIORef drawRef
 	let	cs = cos $ fromIntegral d0 * pi / 180 :: Double
 		sn = sin $ fromIntegral d0 * pi / 180 :: Double
-	(x', y') <- doWhile (fromIntegral x0, fromIntegral y0) $ \(x, y) -> do
-		let	nx = x + 10 * cs
-			ny = y + 10 * sn
-		setForegroundColor xstat 0xffffff
-		displayTurtle xstat (round x) (round y) d0 s
-		setForegroundColor xstat 0x000000
-		displayTurtle xstat (round nx) (round ny) d0 s
-		drawLine dpy win gc x0 y0 (round nx) (round ny)
-		flush dpy
-		threadDelay 30000
-		return ((nx, ny),
-			(nx + 10 * cs - fromIntegral x0) ^ 2 +
-				(ny + 10 * sn - fromIntegral y0) ^ 2 < (fromIntegral len) ^ 2)
+	(x', y') <- if len < 10 then return (fromIntegral x0, fromIntegral y0)
+		else doWhile (fromIntegral x0, fromIntegral y0) $ \(x, y) -> do
+			let	nx = x + 10 * cs
+				ny = y + 10 * sn
+			setForegroundColor xstat 0xffffff
+			displayTurtle xstat (round x) (round y) d0 s
+			setForegroundColor xstat 0x000000
+			drawLines
+			displayTurtle xstat (round nx) (round ny) d0 s
+			drawLine dpy win gc x0 y0 (round nx) (round ny)
+			flush dpy
+			threadDelay 30000
+			return ((nx, ny),
+				(nx + 10 * cs - fromIntegral x0) ^ 2 +
+					(ny + 10 * sn - fromIntegral y0) ^ 2 < (fromIntegral len) ^ 2)
 	setForegroundColor xstat 0xffffff
 	displayTurtle xstat (round x') (round y') d0 s
 	setForegroundColor xstat 0x000000
+	drawLines
 	displayTurtle xstat (x0 + round (fromIntegral len * cs))
 		(y0 + round (fromIntegral len * sn)) d0 s
-	drawLine dpy win gc x0 y0
+	let draw = drawLine dpy win gc x0 y0
 		(x0 + round (fromIntegral len * cs))
 		(y0 + round (fromIntegral len * sn))
+	draw
+	writeIORef drawRef $ drawLines >> draw
 	flush dpy
 	return $ (x0 + round (fromIntegral len * cs),
 		y0 + round (fromIntegral len * sn))
@@ -170,15 +198,18 @@ kameForward xstat@(dpy, win, gc) d0 (x0, y0) len s = do
 kameTurn ::
 	(Display, Window, GC) -> Int -> Int -> (Position, Position) -> Double -> IO Int
 kameTurn xstat@(dpy, _, _) d0 dd (x, y) s = do
-	let nd = d0 + dd
+	drawLines <- readIORef drawRef
+	let	nd = d0 + dd
+		ten = signum dd * 10
 	d' <- doWhile d0 $ \d -> do
 		setForegroundColor xstat 0xffffff
 		displayTurtle xstat x y d s
 		setForegroundColor xstat 0x000000
-		displayTurtle xstat x y (d + 10) s
+		displayTurtle xstat x y (d + ten) s
+		drawLines
 		flush dpy
 		threadDelay 30000
-		return (d + 10, d + 20 < nd)
+		return (d + ten, if ten > 0 then d + 20 < nd else d - 20 > nd)
 	setForegroundColor xstat 0xffffff
 	displayTurtle xstat x y d' s
 	setForegroundColor xstat 0x000000
