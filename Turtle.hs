dc99ef5258e4aa96cdde3deeaa1f6af06cf9f639
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-12-26 09:50:15 +0900

    commit before changes

--- Turtle.hs
+++ Turtle.hs
@@ -10,8 +10,8 @@ import Control.Concurrent
 import System.IO.Unsafe
 import Data.IORef
 
-kameStat :: IORef (Bool, Position, Double)
-kameStat = unsafePerformIO $ newIORef (True, 150, 1)
+kameStat :: IORef (Int, (Position, Position), Double)
+kameStat = unsafePerformIO $ newIORef (0, (150, 150), 1)
 
 xstatRef :: IORef (Display, Window, GC)
 xstatRef = unsafePerformIO $ newIORef (undefined, undefined, undefined)
@@ -93,7 +93,7 @@ setForegroundColor (dpy, _, gc) clr = setForeground dpy gc clr
 
 main = do
 	xstat@(dpy, _, _) <- makeWindow
-	doWhile (True, 150) $ \(b, x) -> allocaXEvent $ \e -> do
+	doWhile (0, 150) $ \(b, x) -> allocaXEvent $ \e -> do
 		nextEvent dpy e
 		ev <- getEvent e
 		case ev of
@@ -106,7 +106,7 @@ main = do
 					't' -> fmap (flip (,) x)
 						$ kameTurn xstat b x 0.4
 					' ' -> fmap ((,) b)
-						$ kameForward xstat b x 100 0.4
+						$ kameForward xstat b (x, 150) 100 0.4
 					_ -> return (b, x)
 				return (ns, ch /= 'q')
 	closeWindow xstat
@@ -114,55 +114,55 @@ main = do
 forward :: Position -> IO ()
 forward dx = do
 	xstat <- readIORef xstatRef
-	(b, x, s) <- readIORef kameStat
-	x' <- kameForward xstat b x dx $ s * 0.3
-	writeIORef kameStat (b, x', s)
+	(b, (x, y), s) <- readIORef kameStat
+	x' <- kameForward xstat b (x, 150) dx $ s * 0.3
+	writeIORef kameStat (b, (x', y), s)
 
 turn :: IO ()
 turn = do
 	xstat <- readIORef xstatRef
-	(b, x, s) <- readIORef kameStat
+	(b, (x, y), s) <- readIORef kameStat
 	b' <- kameTurn xstat b x $ s * 0.3
-	writeIORef kameStat (b', x, s)
+	writeIORef kameStat (b', (x, y), s)
 
 shapeSize :: Double -> IO ()
 shapeSize ns = do
 	xstat@(dpy, _, _) <- readIORef xstatRef
-	(b, x, s) <- readIORef kameStat
+	(b, (x, y), s) <- readIORef kameStat
 	setForegroundColor xstat 0xffffff
-	displayTurtle xstat x 150 (if b then 0 else 180) $ s * 0.3
+	displayTurtle xstat x 150 b $ s * 0.3
 	setForegroundColor xstat 0x000000
-	displayTurtle xstat x 150 (if b then 0 else 180) $ ns * 0.3
+	displayTurtle xstat x 150 b $ ns * 0.3
 	flush dpy
-	writeIORef kameStat (b, x, ns)
+	writeIORef kameStat (b, (x, y), ns)
 
 kameForward ::
-	(Display, Window, GC) -> Bool -> Position -> Position -> Double -> IO Position
-kameForward xstat@(dpy, _, _) b x0 dx s = do
+	(Display, Window, GC) -> Int -> (Position, Position) -> Position -> Double -> IO Position
+kameForward xstat@(dpy, _, _) d0 (x0, y0) dx s = do
 	doWhile x0 $ \x -> do
+		let nx = x + if d0 == 0 then 10 else - 10
 		setForegroundColor xstat 0xffffff
-		displayTurtle xstat (x - if b then 10 else - 10) 150
-			(if b then 0 else 180) s
+		displayTurtle xstat (x - if d0 == 0 then 10 else - 10) 150
+			(if d0 == 0 then 0 else 180) s
 		setForegroundColor xstat 0x000000
-		displayTurtle xstat x 150 (if b then 0 else 180) s
+		displayTurtle xstat x 150 (if d0 == 0 then 0 else 180) s
 		flush dpy
 		threadDelay 30000
-		return (x + if b then 10 else - 10,
-			if b then x < x0 + dx else x > x0 - dx)
-	return $ (x0 + if b then dx else - dx)
+		return (x + if d0 == 0 then 10 else - 10,
+			if d0 == 0 then x < x0 + dx else x > x0 - dx)
+	return $ (x0 + if d0 == 0 then dx else - dx)
 
-kameTurn :: (Display, Window, GC) -> Bool -> Position -> Double -> IO Bool
-kameTurn xstat@(dpy, _, _) b x s = do
-	doWhile (if b then 0 else 180) $ \d -> do
+kameTurn :: (Display, Window, GC) -> Int -> Position -> Double -> IO Int
+kameTurn xstat@(dpy, _, _) d0 x s = do
+	doWhile d0 $ \d -> do
 		setForegroundColor xstat 0xffffff
 		displayTurtle xstat x 150 (d - 10) s
 		setForegroundColor xstat 0x000000
 		displayTurtle xstat x 150 d s
 		flush dpy
 		threadDelay 30000
-		return (d + 10,
-			d < if b then 180 else 360)
-	return $ not b
+		return (d + 10, d < d0 + 180)
+	return $ if d0 == 0 then 180 else 0
 
 getKeyChar :: (Display, Window, GC) -> Event -> IO Char
 getKeyChar (dpy, _, _) ev = do
