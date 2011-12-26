5031d97b1b6061ae7dbdc02c53f8f219b9fa2c0a
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-12-26 09:31:35 +0900

    change turtle size

--- Turtle.hs
+++ Turtle.hs
@@ -10,8 +10,8 @@ import Control.Concurrent
 import System.IO.Unsafe
 import Data.IORef
 
-kameStat :: IORef (Bool, Position)
-kameStat = unsafePerformIO $ newIORef (True, 150)
+kameStat :: IORef (Bool, Position, Double)
+kameStat = unsafePerformIO $ newIORef (True, 150, 1)
 
 xstatRef :: IORef (Display, Window, GC)
 xstatRef = unsafePerformIO $ newIORef (undefined, undefined, undefined)
@@ -42,13 +42,16 @@ closeWindow (dpy, _, _) = closeDisplay dpy
 addPoint :: Point -> Position -> Position -> Point
 addPoint (Point x y) dx dy = Point (x + dx) (y + dy)
 
-makeTurtle :: Position -> Position -> Double -> [Point]
-makeTurtle x y rad =
+makeTurtle :: Position -> Position -> Double -> Double -> [Point]
+makeTurtle x y rad size =
 	map (uncurry $ addPoint $ Point x y)
-		$ map (uncurry $ rotatePoint rad) turtle
+		$ map (rotatePoint rad . mulPoint size) turtle
 
-rotatePoint :: Double -> Position -> Position -> (Position, Position)
-rotatePoint rad x y =
+mulPoint :: Double -> (Position, Position) -> (Position, Position)
+mulPoint size (x, y) = (x `mul` size, y `mul` size)
+
+rotatePoint :: Double -> (Position, Position) -> (Position, Position)
+rotatePoint rad (x, y) =
 	(x `mul` cos rad - y `mul` sin rad, x `mul` sin rad + y `mul` cos rad)
 
 mul :: (Integral a, RealFrac b) => a -> b -> a
@@ -80,9 +83,10 @@ turtle = ttl ++ reverse (map (second $ \x -> - x) ttl)
 reverseY :: Position -> Point -> Point
 reverseY y0 (Point x y) = Point x $ 2 * y0 - y
 
-displayTurtle :: (Display, Window, GC) -> Position -> Position -> Double -> IO ()
-displayTurtle xstat x y r =
-	makeFilledPolygon xstat $ makeTurtle x y r
+displayTurtle ::
+	(Display, Window, GC) -> Position -> Position -> Int -> Double -> IO ()
+displayTurtle xstat x y d s =
+	makeFilledPolygon xstat $ makeTurtle x y (fromIntegral d * pi / 180) s
 
 setForegroundColor :: (Display, Window, GC) -> Pixel -> IO ()
 setForegroundColor (dpy, _, gc) clr = setForeground dpy gc clr
@@ -94,13 +98,15 @@ main = do
 		ev <- getEvent e
 		case ev of
 			ExposeEvent {} -> do
-				displayTurtle xstat 150 150 0
+				displayTurtle xstat 150 150 0 0.4
 				return ((b, x), True)
 			KeyEvent {} -> do
 				ch <- getKeyChar xstat ev
 				ns <- case ch of
-					't' -> fmap (flip (,) x) $ kameTurn xstat b x
-					' ' -> fmap ((,) b) $ kameForward xstat b x 100
+					't' -> fmap (flip (,) x)
+						$ kameTurn xstat b x 0.4
+					' ' -> fmap ((,) b)
+						$ kameForward xstat b x 100 0.4
 					_ -> return (b, x)
 				return (ns, ch /= 'q')
 	closeWindow xstat
@@ -108,42 +114,54 @@ main = do
 forward :: Position -> IO ()
 forward dx = do
 	xstat <- readIORef xstatRef
-	(b, x) <- readIORef kameStat
-	x' <- kameForward xstat b x dx
-	writeIORef kameStat (b, x')
+	(b, x, s) <- readIORef kameStat
+	x' <- kameForward xstat b x dx $ s * 0.3
+	writeIORef kameStat (b, x', s)
 
 turn :: IO ()
 turn = do
 	xstat <- readIORef xstatRef
-	(b, x) <- readIORef kameStat
-	b' <- kameTurn xstat b x
-	writeIORef kameStat (b', x)
+	(b, x, s) <- readIORef kameStat
+	b' <- kameTurn xstat b x $ s * 0.3
+	writeIORef kameStat (b', x, s)
 
-kameForward :: (Display, Window, GC) -> Bool -> Position -> Position -> IO Position
-kameForward xstat@(dpy, _, _) b x0 dx = do
+shapeSize :: Double -> IO ()
+shapeSize ns = do
+	xstat@(dpy, _, _) <- readIORef xstatRef
+	(b, x, s) <- readIORef kameStat
+	setForegroundColor xstat 0xffffff
+	displayTurtle xstat x 150 (if b then 0 else 180) $ s * 0.3
+	setForegroundColor xstat 0x000000
+	displayTurtle xstat x 150 (if b then 0 else 180) $ ns * 0.3
+	flush dpy
+	writeIORef kameStat (b, x, ns)
+
+kameForward ::
+	(Display, Window, GC) -> Bool -> Position -> Position -> Double -> IO Position
+kameForward xstat@(dpy, _, _) b x0 dx s = do
 	doWhile x0 $ \x -> do
 		setForegroundColor xstat 0xffffff
-		displayTurtle xstat (x - if b then 10 else - 10) 150 $
-			if b then 0 else pi
+		displayTurtle xstat (x - if b then 10 else - 10) 150
+			(if b then 0 else 180) s
 		setForegroundColor xstat 0x000000
-		displayTurtle xstat x 150 $ if b then 0 else pi
+		displayTurtle xstat x 150 (if b then 0 else 180) s
 		flush dpy
 		threadDelay 30000
 		return (x + if b then 10 else - 10,
 			if b then x < x0 + dx else x > x0 - dx)
 	return $ (x0 + if b then dx else - dx)
 
-kameTurn :: (Display, Window, GC) -> Bool -> Position -> IO Bool
-kameTurn xstat@(dpy, _, _) b x = do
-	doWhile (if b then 0 else pi) $ \d -> do
+kameTurn :: (Display, Window, GC) -> Bool -> Position -> Double -> IO Bool
+kameTurn xstat@(dpy, _, _) b x s = do
+	doWhile (if b then 0 else 180) $ \d -> do
 		setForegroundColor xstat 0xffffff
-		displayTurtle xstat x 150 $ d - (10 * pi / 180)
+		displayTurtle xstat x 150 (d - 10) s
 		setForegroundColor xstat 0x000000
-		displayTurtle xstat x 150 d
+		displayTurtle xstat x 150 d s
 		flush dpy
 		threadDelay 30000
-		return (d + 10 * pi / 180,
-			d < if b then pi - 10 * pi / 180 else 2 * pi - 10 * pi / 180)
+		return (d + 10,
+			d < if b then 180 else 360)
 	return $ not b
 
 getKeyChar :: (Display, Window, GC) -> Event -> IO Char
