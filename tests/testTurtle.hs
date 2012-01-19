71f25f689259fbe3758aabd3dedabb4e717976f4
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2012-01-19 16:58:09 +0900

    add testTurtle

---
+++ tests/testTurtle.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Turtle
+import System.Random
+import Control.Monad
+import Control.Concurrent
+
+randomTurtle :: IO ()
+randomTurtle = do
+	f <- openField
+	threadDelay 1000000
+	t <- newTurtle f
+	shape t "turtle"
+	shapesize t 3
+	penup t
+	forward t 150
+	pendown t
+	left t 90
+	circle t 150
+	penup t
+	home t
+	pendown t
+	sequence_ $ repeat $ do
+		randomRIO (-180, 180) >>= left t >> forward t 15
+		d <- distance t 0 0
+		when (d > 150) $ undo t
