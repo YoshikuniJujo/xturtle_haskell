1ca91bb378894160ee15aa1a2fbda72ce10dec8e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2012-01-31 11:47:14 +0900

    hackage version 0.0.7

--- src/Graphics/X11/Turtle.hs
+++ src/Graphics/X11/Turtle.hs
@@ -36,7 +36,7 @@ import Data.IORef
 import Control.Arrow(second)
 
 xturtleVersion :: (Int, String)
-xturtleVersion = (1, "0.0.6c")
+xturtleVersion = (1, "0.0.7")
 
 data Turtle = Turtle {
 	inputChan :: Chan TurtleInput,
