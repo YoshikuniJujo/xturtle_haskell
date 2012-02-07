a45d62d6225fb54748ea0f6def5c19f52b6c4c9b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2012-02-07 12:57:44 +0900

    add tests/testOnClick.hs

---
+++ tests/testOnClick.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import Graphics.X11.Turtle
+
+main :: IO ()
+main = do
+	f <- openField
+	t <- newTurtle f
+	shape t "turtle"
+	shapesize t 2
+	pensize t 10
+	pencolor t 0 127 0
+	onclick f $ \x y -> goto t x y >> return True
+	waitField f
+	putStrLn "end"
