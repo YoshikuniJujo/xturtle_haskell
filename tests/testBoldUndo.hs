33e279d32e182699e6e9c2df33ccb0469c9db796
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2012-02-15 10:21:47 +0900

    edit bugs and todo and add a test program

---
+++ tests/testBoldUndo.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Graphics.X11.Turtle
+import Control.Monad
+
+main :: IO ()
+main = do
+	print xturtleVersion
+	f <- openField
+	t <- newTurtle f
+	shape t "turtle"
+	replicateM_ 4 $ do
+		replicateM_ 36 $ forward t 10 >> left t 10
+		right t 90
+	shapesize t 2
+	pensize t 10
+	replicateM_ 4 $ do
+		replicateM_ 36 $ forward t 10 >> left t 10
+		right t 90
+	replicateM_ 600 $ undo t
+	waitField f
