0e52204e5c5727b2db9f416d8e17039e736e1fcc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2012-03-23 14:26:28 +0900

    refactoring Input.hs (not yet finished)

--- src/Graphics/X11/Turtle/Input.hs
+++ src/Graphics/X11/Turtle/Input.hs
@@ -21,38 +21,38 @@ import Graphics.X11.Turtle.State(TurtleState(..), initialTurtleState)
 import Text.XML.YJSVG(SVG(..), Color(..), Position(..))
 
 import Control.Concurrent.Chan(Chan, newChan, getChanContents)
-import Control.Arrow
-import Data.Tuple.Tools
+import Control.Arrow((***))
+import Data.Tuple.Tools(rotate)
 
 --------------------------------------------------------------------------------
 
 data TurtleInput
-	= Shape [(Double, Double)]
-	| Shapesize Double Double
-	| Pencolor Color
-	| Pensize Double
-	| SetPendown Bool
-	| SetFill Bool
-	| SetPoly Bool
-	| SetVisible Bool
-	| Degrees Double
-	| PositionStep (Maybe Double)
-	| DirectionStep (Maybe Double)
-	| Undonum Int
-	| Goto Position
+	= Goto Position
+	| Forward Double
 	| Rotate Double
+	| TurnLeft Double
+	| Dot Double
+	| Stamp
 	| Write String Double String
 	| PutImage FilePath Double Double
-	| Stamp
-	| Dot Double
-	| Bgcolor Color
 	| Undo
+	| Undonum Int
 	| Clear
-	| Forward Double
-	| TurnLeft Double
 	| Sleep Int
-	| SetFlush Bool
 	| Flush
+	| Shape [(Double, Double)]
+	| Shapesize Double Double
+	| Pensize Double
+	| Pencolor Color
+	| Bgcolor Color
+	| SetPendown Bool
+	| SetVisible Bool
+	| SetFill Bool
+	| SetPoly Bool
+	| SetFlush Bool
+	| PositionStep (Maybe Double)
+	| DirectionStep (Maybe Double)
+	| Degrees Double
 	deriving (Show, Read)
 
 turtleSeries :: IO (Chan TurtleInput, [TurtleInput], [TurtleState])
@@ -81,12 +81,49 @@ inputToTurtle tsbs ts0 (ti : tis) =
 	let ts1 = nextTurtle ts0 ti in ts1 : inputToTurtle (ts0 : tsbs) ts1 tis
 inputToTurtle _ _ [] = error "no more input"
 
+clearState :: TurtleState -> TurtleState
+clearState t = t{draw = Nothing, clear = False, undo = False, undonum = 1,
+	sleep = Nothing, flush = False}
+
 nextTurtle :: TurtleState -> TurtleInput -> TurtleState
+nextTurtle t (Goto pos) = (clearState t){
+	position = pos, draw = drw, drawed = maybe id (:) drw $ drawed t,
+	fillPoints = (if fill t then (pos :) else id) $ fillPoints t,
+	polyPoints = (if poly t then (pos :) else id) $ polyPoints t}
+	where
+	drw = if not $ pendown t then Nothing
+		else Just $ Line pos (position t) (pencolor t) (pensize t)
+nextTurtle t (Rotate dir) = (clearState t){direction = dir * 2 * pi / degrees t}
+nextTurtle t (Dot sz) = (clearState t){
+	draw = Just drw, drawed = drw : drawed t}
+	where
+	drw = Rect (position t) sz sz 0 (pencolor t) (pencolor t)
+nextTurtle t Stamp = (clearState t){
+	draw = Just drw, drawed = drw : drawed t}
+	where
+	Center x0 y0 = position t
+	sp = let (sx, sy) = shapesize t in
+		map (((+ x0) *** (+ y0)) . rotate (direction t) . ((* sx) *** (* sy))) $ shape t
+	points = map (uncurry Center) sp
+	drw = Polyline points (pencolor t) (pencolor t) 0
+nextTurtle t (Write fnt sz str) = (clearState t){
+	draw = Just txt, drawed = txt : drawed t}
+	where txt = Text (position t) sz (pencolor t) fnt str
+nextTurtle t (PutImage fp w h) = (clearState t){
+	draw = Just img, drawed = img : drawed t}
+	where img = Image (position t) w h fp
+nextTurtle t (Undonum un) = (clearState t){undonum = un}
+nextTurtle t Clear = (clearState t){clear = True, drawed = [last $ drawed t]}
+nextTurtle t (Sleep time) = (clearState t){sleep = Just time}
+nextTurtle t Flush = (clearState t){flush = True}
 nextTurtle t (Shape sh) = (clearState t){shape = sh}
 nextTurtle t (Shapesize sx sy) = (clearState t){shapesize = (sx, sy)}
-nextTurtle t (Pencolor c) = (clearState t){pencolor = c}
 nextTurtle t (Pensize ps) = (clearState t){pensize = ps}
+nextTurtle t (Pencolor c) = (clearState t){pencolor = c}
+nextTurtle t (Bgcolor c) = (clearState t){
+	draw = Just $ Fill c, drawed = init (drawed t) ++ [Fill c]}
 nextTurtle t (SetPendown pd) = (clearState t){pendown = pd}
+nextTurtle t (SetVisible v) = (clearState t){visible = v}
 nextTurtle t (SetFill f) = (clearState t){
 	fill = f,
 	draw = if fill t && not f then Just fl else Nothing,
@@ -97,46 +134,8 @@ nextTurtle t (SetFill f) = (clearState t){
 nextTurtle t (SetPoly p) = (clearState t){
 	poly = p,
 	polyPoints = if p then [position t] else polyPoints t}
-nextTurtle t (SetVisible v) = (clearState t){visible = v}
-nextTurtle t (Degrees ds) = (clearState t){degrees = ds}
+nextTurtle t (SetFlush ss) = (clearState t){stepbystep = ss}
 nextTurtle t (PositionStep ps) = (clearState t){positionStep = ps}
 nextTurtle t (DirectionStep ds) = (clearState t){directionStep = ds}
-nextTurtle t (Undonum un) = (clearState t){undonum = un}
-nextTurtle t (Goto pos) = (clearState t){
-	position = pos,
-	draw = if pendown t then Just ln else Nothing,
-	drawed = if pendown t then ln : drawed t else drawed t,
-	fillPoints = if fill t then pos : fillPoints t else fillPoints t,
-	polyPoints = if poly t then pos : polyPoints t else polyPoints t}
-	where
-	ln = Line pos (position t) (pencolor t) (pensize t)
-nextTurtle t (Rotate d) = (clearState t){direction = d * 2 * pi / degrees t}
-nextTurtle t (Write fnt sz str) = (clearState t){
-	draw = Just txt, drawed = txt : drawed t}
-	where txt = Text (position t) sz (pencolor t) fnt str
-nextTurtle t (PutImage fp w h) = (clearState t){
-	draw = Just img, drawed = img : drawed t}
-	where img = Image (position t) w h fp
-nextTurtle t Stamp = (clearState t){
-	draw = Just stamp, drawed = stamp : drawed t}
-	where
-	Center x0 y0 = position t
-	sp = let (sx, sy) = shapesize t in
-		map (((+ x0) *** (+ y0)) . rotate (direction t) . ((* sx) *** (* sy))) $ shape t
-	points = map (uncurry Center) sp
-	stamp = Polyline points (pencolor t) (pencolor t) 0
-nextTurtle t (Dot sz) = (clearState t){
-	draw = Just dot, drawed = dot : drawed t}
-	where
-	dot = Rect (position t) sz sz 0 (pencolor t) (pencolor t)
-nextTurtle t (Bgcolor c) = (clearState t){
-	draw = Just $ Fill c, drawed = init (drawed t) ++ [Fill c]}
-nextTurtle t Clear = (clearState t){clear = True, drawed = [last $ drawed t]}
-nextTurtle t (Sleep time) = (clearState t){sleep = Just time}
-nextTurtle t (SetFlush ss) = (clearState t){stepbystep = ss}
-nextTurtle t Flush = (clearState t){flush = True}
+nextTurtle t (Degrees ds) = (clearState t){degrees = ds}
 nextTurtle _ _ = error "not defined"
-
-clearState :: TurtleState -> TurtleState
-clearState t = t{undonum = 1, undo = False, clear = False, draw = Nothing,
-	sleep = Nothing, flush = False}
